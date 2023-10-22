0ef050ffabe50495c71f610f367a780226af0f8d
caozhiy <1030434805@qq.com>
2023-10-22 22:48:57 +0800

    Use Socket to transmit txt files and practice the I/O operations.

--- src/edu/jlu/MySocket/MyClient.java
+++ src/edu/jlu/MySocket/MyClient.java
@@ -1,37 +1,78 @@
 package edu.jlu.MySocket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class MyClient {
-    public static void main(String[] args){
-        FirstTest();
+    public static void main(String[] args) {
+//        FirstTest();
+        TransmitFile();
     }
 
     public static void FirstTest() {
+
+        OutputStreamWriter writer = null;
+        BufferedReader reader = null;
         try (Socket socket = new Socket("localhost", 8080);
              Scanner in = new Scanner(System.in)) {
             System.out.println("已经连接成功!");
             System.out.print("请输入需要写入的数据:  ");
-            OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+            writer = new OutputStreamWriter(socket.getOutputStream());
             writer.write(in.nextLine() + "\n");
             writer.flush();
             System.out.println("数据已发送!");
 
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("客户端收到的数据是:\t" + reader.readLine());
+
 
         } catch (IOException e) {
             System.out.println("连接客户端失败");
             e.printStackTrace();
         } finally {
+
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("客户端结束运行!");
         }
+    }
+
+    public static void TransmitFile() {
+
+        OutputStream output = null;
+        try (Socket socket = new Socket("localhost", 8080);
+             BufferedInputStream input = new BufferedInputStream(new FileInputStream("src/resource/test.txt"))) {
+            output = socket.getOutputStream();
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = input.read(bytes)) != -1) {
+                output.write(bytes, 0, len);
+            }
+            output.flush();
+            System.out.println("客户端传输完毕!");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (output != null)
+                    output.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("客户端停止运行......");
+        }
+
     }
 }
--- src/edu/jlu/MySocket/MyServer.java
+++ src/edu/jlu/MySocket/MyServer.java
@@ -1,11 +1,15 @@
 package edu.jlu.MySocket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MyServer {
     public static void main(String[] args) {
+        TransmitFile();
+    }
+
+    public static void FirstConnect() {
         try (ServerSocket server = new ServerSocket(8080)) {
             System.out.println("正在等待客户端连接……");
             Socket client = server.accept();
@@ -15,4 +19,42 @@ public class MyServer {
             throw new RuntimeException(e);
         }
     }
+
+    public static void TransmitFile() {
+
+        Socket client = null;
+        BufferedInputStream input = null;
+
+        try (ServerSocket server = new ServerSocket(8080);
+             FileOutputStream output =new FileOutputStream("src\\resource\\aim.txt")) {
+            {
+                System.out.println("服务器正在等待连接......");
+                client = server.accept();
+                System.out.println("客户端已连接服务器, IP地址:  " + client.getInetAddress().getHostAddress());
+                input = new BufferedInputStream(client.getInputStream());
+
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = input.read(bytes)) != -1) {
+                    output.write(bytes, 0, len);
+                }
+                System.out.println("服务器传输完毕!");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (client != null)
+                    client.close();
+                if (input != null) {
+                    input.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("服务器停止运行......");
+        }
+    }
 }
+
